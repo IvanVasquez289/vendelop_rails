2ef726cbf01f88e25c1998ba8e7b6289cb3e0367
ivan <ivanjv1234@gmail.com>
2025-04-09 16:40:50 -0600

    create method

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -18,4 +18,15 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "form"
   end
+
+  test "create a new product" do
+    post products_path, params: {
+      product: {
+        title: "Canasta de madera",
+        description: "Muy buena calidad",
+        price: 100
+      }
+    }
+    assert_redirected_to products_path
+  end
 end
