15e46fcb5765020b024c4cc6780fe1c135d00c86
ivan <ivanjv1234@gmail.com>
2025-04-09 21:24:49 -0600

    flash

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -28,6 +28,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
       }
     }
     assert_redirected_to products_path
+    assert_equal "El producto se ha creado correctamente", flash[:notice]
   end
 
   test "does not allow to create a new product with empty fields" do
