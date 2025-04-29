351f0af76e03fcbebf9a79ae8a94be97b28e4b30
ivan <ivanjv1234@gmail.com>
2025-04-28 23:44:34 -0600

    translate

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module Vendelop
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # available lenguages
+    config.i18n.available_locales = [ :en, :es ]
+    # default lenguage
+    config.i18n.default_locale = :es
   end
 end
